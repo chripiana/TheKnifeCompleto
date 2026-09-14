fa83e46ccc199531ca26d58c9ef2bd990c3eb327
chripiana <81437944+chripiana@users.noreply.github.com>
2026-09-14 20:17:58 +0200

    Delete bin directory

--- bin/run-client.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-IF EXIST "C:\javafx-sdk-21\lib" (
-  java --module-path "C:\javafx-sdk-21\lib" --add-modules=javafx.controls,javafx.fxml -jar "%~dp0ClientTK.jar"
-) ELSE (
-  java -jar "%~dp0ClientTK.jar"
-)
